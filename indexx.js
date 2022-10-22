b85300b1c5370225e23f870fae4f1eb18711bb67
Temitope Adejolu <tadejolu@gmail.com>
2022-10-22 21:00:09 +0100

    hamburger

---
+++ indexx.js
@@ -0,0 +1,8 @@
+let myArr = ['extravagant', 'worship','gg'];
+
+function rendering(myArr) {
+    return myArr.toString();
+    }
+
+
+console.log(rendering(myArr));
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Nav/Navbar";
+import Todo from "./Todo/Todo";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Todo></Todo>
+      App
+    </>
+  );
+}
+
+export default App;
